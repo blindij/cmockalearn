7afb2e287932f355e7770ff5c836442ff8906ea8
Bjoern Lindi <blindij@users.noreply.github.com>
2018-08-06 08:02:18 +0200

    Test of showhistory() builds, but fails.
    Modified CMakeLists.txt. C-compiler set to clang.
    Modified test.cmake to use __wrap_getdata().
    Corrected errors in test_showhistory.c

--- robbins/README.md
+++ robbins/README.md
@@ -1,5 +1,4 @@
 Examples from Robbins: "Unix Systems Programming: Communication, Concurrency and Threads". Trying to combine examples with
 CMocka. listlib.h is describe on page 33 - last example started on.
 
-Have made mock test using __wrap_accesslist(). Next step is to call actual accesslist(), and then make mock tests for the next function
- - adddata(data_t data)
+Next is to fully implement showhistory(). A mock function for getkey() needs to be written.
--- robbins/test/test_showhistory.c
+++ robbins/test/test_showhistory.c
@@ -2,6 +2,20 @@
  * File: test_showhistory.c
  */
 
+int __wrap_getdata(int key, data_t *datap) {
+
+/*
+ * getdata() fill in the passed data struct with data
+ * from the list at the key. getdata() returns:
+ * -1  - when it is unable to allocate space for the string
+ *  0  - on success
+ */
+   check_expected(key);
+   check_expected(datap);
+
+   return 0;
+}
+
 /*
  * showhistory() - example from page 37, Robbins. 
  * This is a function in the keeploglib.c
@@ -16,37 +30,12 @@ static void showhistory(int *r){
       fprintf(stderr, "No history\n");
       *r = -1;
    }
-//   while(!getdata(key, &data) && (data.string != NULL)) {
-//      fprintf(f,
-//   }
+   while(!getdata(key, &data) && (data.string != NULL)) {
+      // fprintf(stderr,"Command: %s\nTime: %s\n", data.string, ctime(&(data.time));
+      fprintf(stderr,"Command: %s\nTime: %d\n", data.string, data.t);
+      free(data.string);
+   }
 }
-
-//int __wrap_accessdata(void){
-//    bool isempty;
-//    bool cannotallocate;
-//    bool firsttimeaccess;
-//    bool nextkeyis;
-//    int tmp;
-//    // check_expected_ptr( <somevariable>)
-//
-//    isempty = mock_type(bool);
-//    if ( isempty == true) {
-//        return -1;
-//    }
-//
-//    cannotallocate = mock_type(bool);
-//    if ( cannotallocate == true) {
-//        return -2;
-//    }
-//
-//    firsttimeaccess = mock_type(bool);
-//    if ( firsttimeaccess == true) {
-//        return 0;
-//    }
-//    return mock_type(int);
-//}
-//
-//
 
 static void test_showhistory(void **state){
    (void) state;  /* unused */
