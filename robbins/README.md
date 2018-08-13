f9e746cc038a0de730971179ed5d0a87899861b0
Bjoern Lindi <blindij@users.noreply.github.com>
2018-08-13 13:14:09 +0200

    Testing showhistory() with mocked function getdata().
    getdata() returns 1  after second entry.

--- robbins/README.md
+++ robbins/README.md
@@ -1,5 +1,5 @@
 Examples from Robbins: "Unix Systems Programming: Communication, Concurrency and Threads". Trying to combine examples with
 CMocka. listlib.h is describe on page 33 - last example started on.
 
-Next is to fully implement showhistory(). A mock function for getkey() needs to be written.
-Error message point to not enough mock objects.
+Next is to fully implement showhistory(). Two data items are written out. How to stop properly,
+data.string=NULL,...
--- robbins/test/test_showhistory.c
+++ robbins/test/test_showhistory.c
@@ -16,10 +16,16 @@ int __wrap_getdata(int key, data_t *datap) {
     //datap->string = (char *)malloc(strlen(item.string+1));
     //strcpy(datap->string, &item.string);
    //datap->t = mock_type(int);
-   datap->t = key;
-   param = mock_ptr_type(char *);
-   datap->string = strdup(param);
-   return mock_type(int);;
+   if (key == -2) {
+       datap->t = key;
+       printf("Assignment of NULL\n");
+       datap->string = NULL;
+   } else {
+       datap->t = key;
+       param = mock_ptr_type(char *);
+       datap->string = strdup(param);
+   }
+   return mock_type(int);
 }
 
 /*
@@ -37,26 +43,11 @@ static void showhistory(int *r){
       fprintf(stderr, "No history\n");
       return;
    }
-   tmp = getdata(key,&data);
-   if (tmp == 0){
-       fprintf(stderr,"key: %d: %d %s|\n", key, data.t, data.string);
-   } else {
-       fprintf(stderr,"getdata() returns: %d. Key was: %d\n", tmp,key);
+   while(!getdata(key, &data) && (data.string != NULL)) {
+      // fprintf(stderr,"Command: %s\nTime: %s\n", data.string, ctime(&(data.time));
+      fprintf(stderr,"Command: %s\nTime: %d\n", data.string, data.t);
+      //free(data.string);
    }
-//   while(!getdata(key, &data) && (data.string != NULL)) {
-//      // fprintf(stderr,"Command: %s\nTime: %s\n", data.string, ctime(&(data.time));
-//      fprintf(stderr,"Command: %s\nTime: %d\n", data.string, data.t);
-//      free(data.string);
-//   }
-//   return;
-}
-
-static void test_showhistory_no_history(void **state){
-   (void) state;  /* unused */
-   int rv;
-   will_return(__wrap_accessdata,true);
-   showhistory(&rv);
-   assert_int_equal(-1,rv);
 }
 
 static void test_showhistory(void **state){
@@ -73,7 +64,40 @@ static void test_showhistory(void **state){
 //    will_return(__wrap_getdata, 24);
     will_return(__wrap_getdata, 0);
 
+    expect_value(__wrap_getdata, key, 12);
+//    will_return(__wrap_accessdata,false);
+//    will_return(__wrap_accessdata,false);
+//    will_return(__wrap_accessdata,false);
+//    will_return(__wrap_accessdata,12);
+    will_return(__wrap_getdata, cast_ptr_to_largest_integral_type("hokuspokus"));
+     will_return(__wrap_getdata, 0);
+    
+    expect_value(__wrap_getdata, key, 12);
+    //will_return(__wrap_accessdata, false);
+    //will_return(__wrap_accessdata, false);
+    //will_return(__wrap_accessdata, false);
+    // will_return(__wrap_accessdata, 12);
+    will_return(__wrap_getdata, cast_ptr_to_largest_integral_type("morrison"));
+    will_return(__wrap_getdata, 1);
+ 
+//    expect_value(__wrap_getdata, key, -2);
+////    will_return(__wrap_accessdata,false);
+////    will_return(__wrap_accessdata,false);
+////    will_return(__wrap_accessdata,false);
+////    will_return(__wrap_accessdata,22);
+//    //will_return(__wrap_getdata, NULL);
+//    will_return(__wrap_getdata, 0);
+//
     showhistory(&rv);
     assert_int_equal(12,rv);
 
 }
+
+static void test_showhistory_no_history(void **state){
+   (void) state;  /* unused */
+   int rv;
+   will_return(__wrap_accessdata,true);
+   showhistory(&rv);
+   assert_int_equal(-1,rv);
+}
+
