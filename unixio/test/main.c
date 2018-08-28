aecbaff6f0599d5915ccb35fcf5c7e2883668005
Bjoern Lindi <blindij@users.noreply.github.com>
2018-08-28 11:17:40 +0200

    Test of readline(), reads a line of 72 char(bytes).
    The test use open() and close().

--- unixio/test/main.c
+++ unixio/test/main.c
@@ -16,6 +16,7 @@
 int main(void){
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_readline_null),
+        cmocka_unit_test(test_readline),
     };
 
     return cmocka_run_group_tests(tests, NULL,NULL);
--- unixio/test/test_iolib.c
+++ unixio/test/test_iolib.c
@@ -1,3 +1,6 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
  // int readline(int fd, char *buf, int nbytes);
 /*
@@ -5,4 +8,15 @@
  */
  static void test_readline_null(void **state) {
     (void) state;  /* unused */
+ }
+
+ static void test_readline(void **state) {
+    (void) state; /* unused */
+    int bytesread, myfd;
+    char mybuf[100];
+
+    myfd = open("tmp/nintynine.txt", O_RDONLY);
+    bytesread = readline(myfd, mybuf, sizeof(mybuf));
+    close(myfd);
+    assert_int_equal(72,bytesread);
  }
