8055fd4ebd55a45b66c9b6f2df1b40f8537da7e4
Bjoern Lindi <blindij@users.noreply.github.com>
2018-08-29 15:40:05 +0200

    test of ising_initialize_data() creates file 'tmp/initdata'.
    1250 doubles (64 bytes) are written to the file.

--- unixio/test/test_initialize.c
+++ unixio/test/test_initialize.c
@@ -1,6 +1,11 @@
 #define ALLOC(s) calloc ( (s),sizeof(char) )
 #define FREE(s) free(s)
+
+#include <errno.h>
+#include <unistd.h>
+
 void test_initialize(void **state){
+    int bw,myfd;
     size_t nN = N;
     size_t nK = K;
     bool *s;
@@ -12,6 +17,14 @@ void test_initialize(void **state){
     real_t *J = ALLOC ( K*N*N * sizeof(real_t) );
 
     ising_initialize_data(g,nN,nK,Texp,J, s);
+
+    myfd = open("tmp/initdata", O_CREAT | O_WRONLY);
+    //bw = write(myfd,J,64);
+    bw = write(myfd,J,K*N*N*sizeof(real_t));
+    if ( bw == -1) printf("Errno: %d\n", errno);
+    close(myfd);
+
     FREE(J);
     FREE(s);
+    assert_int_equal(10000,bw);
 }
