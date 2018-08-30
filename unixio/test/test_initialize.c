220527566e8f04b579d2a2017886548255607828
Bjoern Lindi <blindij@users.noreply.github.com>
2018-08-30 08:23:52 +0200

    test_initialze.c creates a file with 12500 doubles.
    The block with 1250 doubles is written ten times.

--- unixio/test/test_initialize.c
+++ unixio/test/test_initialize.c
@@ -5,10 +5,11 @@
 #include <unistd.h>
 
 void test_initialize(void **state){
-    int bw,myfd;
+    int i,bw,myfd,totalwritten;
     size_t nN = N;
     size_t nK = K;
     bool *s;
+    totalwritten = 0;
     real_t g = 2.0;
     real_t Texp = 6.0;
     real_t T = floor(pow(10,Texp));
@@ -19,12 +20,14 @@ void test_initialize(void **state){
     ising_initialize_data(g,nN,nK,Texp,J, s);
 
     myfd = open("tmp/initdata", O_CREAT | O_WRONLY);
-    //bw = write(myfd,J,64);
-    bw = write(myfd,J,K*N*N*sizeof(real_t));
-    if ( bw == -1) printf("Errno: %d\n", errno);
+    for(i=0;i<10;i++) {
+        bw = write(myfd,J,K*N*N*sizeof(real_t));
+        if ( bw == -1) printf("Errno: %d\n", errno);
+        totalwritten += bw;
+    }
     close(myfd);
 
     FREE(J);
     FREE(s);
-    assert_int_equal(10000,bw);
+    assert_int_equal(100000,totalwritten);
 }
