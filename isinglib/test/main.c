b03a7e3fa53060806f3c0d6ac0a1893fb25a0664
Bjoern Lindi <blindij@users.noreply.github.com>
2018-10-03 11:51:02 +0200

    Implement ising methods in own library (isinglib).
    Test runs succesful. Next step is to let unixio and
    hdf5 tests use the library.

---
+++ isinglib/test/main.c
@@ -0,0 +1,24 @@
+#include <errno.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include "ising_main.h"
+#include "ising_parameters.h"
+#include "iolib.h"
+
+#include "test_initialize.c"
+
+int main(void){
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(test_initialize),
+    };
+
+    return cmocka_run_group_tests(tests, NULL,NULL);
+}
+
